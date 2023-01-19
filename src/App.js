6e455fb965325a2e182e1f5063775b76615e3211
JorgeAChaconL <chaconlealj@gmail.com>
2023-01-19 12:38:38 -0600

    changes  to solve blank page

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
         </Container>
       </Navbar>
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       
     </Router>
