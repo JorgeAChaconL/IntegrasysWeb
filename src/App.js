3c56bff8f080d07d11f40198266d4ddddd7a078e
JorgeAChaconL <chaconlealj@gmail.com>
2023-01-19 14:30:53 -0600

    Hello world Test

--- src/App.js
+++ src/App.js
@@ -13,62 +13,66 @@ import Home from "./containers/home/Home";
 class App extends Component {
   render() {
     return (
-      <>
-        <Router>
-          <Navbar
-            collapseOnSelect
-            expand="lg"
-            bg="dark"
-            className="mt-10"
-            variant="dark"
-            align="center"
-          >
-            <Container>
-              <Link to="/">
-                <Navbar.Brand>
-                  <img
-                    alt=""
-                    src={isologo}
-                    width="250"
-                    height="auto"
-                    className="d-inline-block align-top "
-                  />{" "}
-                </Navbar.Brand>
-              </Link>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <NavLink
-                    style={({ isActive }) =>
-                      isActive
-                        ? {
-                            textAlign: "center",
-                            color: "#fff",
-                            marginLeft: 10,
-                            marginRight: 10,
-                            textDecoration: "none",
-                          }
-                        : {
-                            textAlign: "center",
-                            color: "#fff",
-                            marginLeft: 10,
-                            marginRight: 10,
-                            textDecoration: "none",
-                          }
-                    }
-                    to=""
-                  >
-                    Home
-                  </NavLink>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </>
+     
+      <h1>
+        Hello world
+      </h1>
+      // <>
+      //   <Router>
+      //     <Navbar
+      //       collapseOnSelect
+      //       expand="lg"
+      //       bg="dark"
+      //       className="mt-10"
+      //       variant="dark"
+      //       align="center"
+      //     >
+      //       <Container>
+      //         <Link to="/">
+      //           <Navbar.Brand>
+      //             <img
+      //               alt=""
+      //               src={isologo}
+      //               width="250"
+      //               height="auto"
+      //               className="d-inline-block align-top "
+      //             />{" "}
+      //           </Navbar.Brand>
+      //         </Link>
+      //         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      //         <Navbar.Collapse id="responsive-navbar-nav">
+      //           <Nav className="justify-content-end flex-grow-1 pe-3">
+      //             <NavLink
+      //               style={({ isActive }) =>
+      //                 isActive
+      //                   ? {
+      //                       textAlign: "center",
+      //                       color: "#fff",
+      //                       marginLeft: 10,
+      //                       marginRight: 10,
+      //                       textDecoration: "none",
+      //                     }
+      //                   : {
+      //                       textAlign: "center",
+      //                       color: "#fff",
+      //                       marginLeft: 10,
+      //                       marginRight: 10,
+      //                       textDecoration: "none",
+      //                     }
+      //               }
+      //               to=""
+      //             >
+      //               Home
+      //             </NavLink>
+      //           </Nav>
+      //         </Navbar.Collapse>
+      //       </Container>
+      //     </Navbar>
+      //     <Routes>
+      //       <Route exact path="/" element={<Home />} />
+      //     </Routes>
+      //   </Router>
+      // </>
     );
   }
 }
